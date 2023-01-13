4a3e80fe0875cd141588db32fa2f7cd43732dd70
mdzubaidul <freelancerjami@gmail.co>
2023-01-13 23:11:09 +0600

    10 login

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 11. save Cart Items
     1. install js-cookie
     2. save and retrieve Cart Items in cookie
+12. Create Login Form
+    1. install react-hook-form
+    2. create input box
+    3. add login button
---
+++ pages/login.js
@@ -0,0 +1,71 @@
+import React from "react";
+import Layout from "../components/Layout";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+
+export default function login() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <Layout title="Login">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mx-auto max-w-screen-md"
+      >
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            {...register("email", {
+              required: "Please enter email",
+              pattern: {
+                value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+                message: "Please enter valid email",
+              },
+            })}
+            className="w-full"
+            id="email"
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            {...register("password", {
+              required: "Please enter password",
+              minLength: { value: 6, message: "password is more than 5 chars" },
+            })}
+            className="w-full"
+            id="password"
+            autoFocus
+          ></input>
+          {errors.password && (
+            <div className="text-red-500 ">{errors.password.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <button type="submit" className="primary-button w-full">
+            Login
+          </button>
+        </div>
+        <div className="mb-4">
+          Don &apos;t have an account? &apos;
+          <Link href="/register">Register</Link>
+        </div>
+      </form>
+    </Layout>
+  );
+}
