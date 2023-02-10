d4aff445354b88dc5bcd8c88ebb18f81c952fcfb
mdzubaidul <freelancerjami@gmail.co>
2023-02-10 20:54:12 +0600

    update profile

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,6 +1,6 @@
 import { useSession } from "next-auth/react";
 import React from "react";
-import Layout from "../components/Layout";
+import Layout from "../../components/Layout";
 
 export default function AdminScreen() {
   const { status, data: session } = useSession();
