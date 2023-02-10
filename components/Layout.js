ffc340cbe83a23f6ed2dd144079fc1a38167d145
mdzubaidul <freelancerjami@gmail.co>
2023-02-10 21:48:44 +0600

    update profile

--- components/Layout.js
+++ components/Layout.js
@@ -77,20 +77,6 @@ const Layout = ({ title, children }) => {
                           </DropdownLink>
                         )}
                       </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <DropdownLink
-                            className={`${
-                              active
-                                ? "bg-violet-500 text-white"
-                                : "text-gray-900"
-                            } group flex w-full items-center rounded-md px-2 py-2 text-sm`}
-                            href="/admin"
-                          >
-                            Admin
-                          </DropdownLink>
-                        )}
-                      </Menu.Item>
 
                       <Menu.Item>
                         {({ active }) => (
